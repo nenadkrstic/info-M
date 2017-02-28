1c17a23eeb5634948df0a3a3e760a1f3b3334690
nenadkrstic <nenadaleksandrakrstic@gmail.com>
2017-02-28 09:40:59 +0100

    Update PageController.php

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Session;
 class PageController extends Controller
 {
 
-  //Vraca index stranu sa vestima , pos. 10 najcitanijih vesti, posednjih 10 komentara
+  //Vraca index stranu sa vestima , poslednjih 10 najcitanijih vesti i poslednjih 10 komentara
     public function pocetna()
     {
         $news = DB::table('news')->orderby('id','desc')->limit(9)->get();
@@ -21,6 +21,7 @@ class PageController extends Controller
         $naj_vest =DB::table('news')->orderBy('naj_vest','desc')->limit(7)->get();
         return view('pocetna',compact('news','comments','naj_vest'));
     }
+  
     //Vraca celu vest i podize inkremen na polje najcitenje vesti (naj_vest)
     public function celaVest($id)
     {
@@ -28,6 +29,7 @@ class PageController extends Controller
         $celaVest = News::findOrFail($id);
         return view('celaVest',compact('celaVest'));
     }
+  
     //Izloguj se
     public function logout()
     {
@@ -35,12 +37,14 @@ class PageController extends Controller
          Session::flash('logOut','Hvala na poseti!!!');
         return redirect('/pocetna');
     }
+  
    //AJax pretraga vraca request sa naslovom iz tabele vesti
     public function search_news()
     {
             if (isset($_GET['news'])) {
             $news = $_GET['news'];
             if(!empty($news)){
+              //Ocistiti formu od nezeljenih unosa
             $rez = News::select('title')->where('status', 'like', "$news")->limit(10)->get();
             foreach ($rez as $r) {
                 echo "<a href='celaVest/$r->id' >" .$r->title . '</a><br>';
@@ -48,6 +52,7 @@ class PageController extends Controller
           }
         }
     }
+  
   //vraca celu vest iz pretrage u novi view
     public function pretraga(Request $request) {
         $pretraga = Request::only('pretraga');
